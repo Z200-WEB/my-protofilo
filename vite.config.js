dd69f6b7a815697d55d0efbb9788a6150421802d
Z200-WEB <doomsday77923@gmail.com>
2026-02-04 20:27:25 +0900

    Update base path to new repo name

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-protofilo/',
+  base: '/my-portofilo/',
   plugins: [react()],
 })
